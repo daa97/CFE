524d1e952bf0a680ed35ebd0855507a53ab49d76
daa97 <114694180+daa97@users.noreply.github.com>
2022-10-30 13:01:26 -0400

    Big Cryogenic Update
    Contains lots of changes. Updated csv tables now include cryogenic fuels over a limited pressure span.

--- Thermo Cycle.py
+++ Thermo Cycle.py
@@ -1,42 +1,112 @@
 from fluids import *
 
 import matplotlib.pyplot as plt
+def moment(mass, R1, R2):
+    return (1/2) * mass * (R1**2 + R2**2)
 
-q_nozzle = 1e4
-dP_pump = 10
-dh_turbo = -4e3 
-y_turb = 0.8            # fraction going into turbopump
-q_regen1 = 2e3          # bypass heating states 3-6
-q_regen2 = 5e3          # pre-cfe heating states 5-8
-dh_cfeturb = -2e3
-dp_pm = -.09
-q_uran = 3e4
+def mass(R1, R2, length, density):
+    return np.abs(np.pi * (R2**2 - R1**2) * length) * density
 
+l = 0.84
+r1 = 0.03
+r2 = 0.045
+rho_u = 17100
+void_frac = 0.5
+omega = 7000 * (2 * np.pi / 60)
+t_init = 10 # seconds for spin-up
+
+rho_uh = (1 - void_frac) * rho_u
+mass_u = mass(r1, r2, l, rho_uh)
+moment_u = moment(mass_u, r1, r2)
+ke_cfe = (1/2) * moment_u * omega**2
+cfe_power = ke_cfe / t_init
+cfe_flow = 0.1
+
+P_tank = 1.5e5
+T_tank = 40
+p_core = 5e6
+T_core = 5000
+T_pm = 800
+dp_uran = -2.2e6
+dp_pm = -9e3
+
+eta_turbopump =1
+eta_cfeturb =1
+
+
+dh_cfeturb = - cfe_power / cfe_flow
+q_uran = 3e7
+
+dh_turbo = -6e6
+q_nozzle = 8e5
+
+y_turb = 1            # fraction going into turbopump
+dh_pump = -dh_turbo * eta_turbopump * y_turb
+
+q_regen1 = 1e6 *0         # bypass heating states 3-6
+q_regen2 = 1e6 *0         # pre-cfe heating states 5-8
 y_throt = 1-y_turb
+
 H2 = Fluid("Hydrogen", prop_files)
 
-start = H2(P=0.1, T=200)                                # state 1
-pumped = H2(s=start.s, P=start.P+dP_pump)               # state 2
-regen = H2(h=pumped.h+q_nozzle, P=pumped.P)             # state 3
-turbo = H2(h=regen.h+dh_turbo, s=regen.s)               # state 4
-bypass = H2(h=regen.h+q_regen1, P=regen.P)              # state 6
-throt = H2(h=bypass.h, P=turbo.P)                        # state 7
-mix = H2(h=y_turb*turbo.h+y_throt*throt.h, P=turbo.P)   # state 5
-hot = H2(h=mix.h+q_regen2, P=mix.P)                     # state 8
-cfe = H2(h=hot.h+dh_cfeturb, s=hot.s)                   # state 9
-pm = H2(h=cfe.h, P=cfe.P+dp_pm)                         # state 10
-core = H2(h=pm.h+q_uran, P=pm.P)                        # state 11
+core = H2(T=T_core, P=p_core)                           # state 11
+pm = H2(T=T_pm, P=core.P - dp_uran)                     # state 10
+cfe = H2(h=pm.h, P=pm.P - dp_pm)                        # state 9
+hot_isent = H2(h=cfe.h-dh_cfeturb, s=cfe.s)             # state 8
+hot = H2(h=cfe.h-dh_cfeturb*eta_cfeturb, P=hot_isent.P) 
+mix = H2(h=hot.h-q_regen2, P=hot.P)                     # state 8
+turbo = H2(h=mix.h, P=mix.P)                # state 4
+start = H2(P=P_tank, T=T_tank)  
+print(start)
+#tfunc1 = H2.s.table.t_func_p(Y1=start.s)
+#tfunc2 = H2.s.table.t_func_p(Y1=turbo.s)
+#regen = H2(h=turbo.h-dh_turbo, s=turbo.s)               # state 3
 
-flow1 = [start, pumped, regen, bypass, throt, mix, hot, cfe, pm, core]
-flow2 = [start, pumped, regen, turbo, mix, hot, cfe, pm, core]
+while True:
+    regen = H2(h=turbo.h-dh_turbo, s=turbo.s)               # state 3
+    pumped = H2(P=regen.P, s=start.s)
+    hp = pumped.h - start.h
+    res = hp + dh_turbo
+    print("RESIDUAL:", res)
+    if abs(res) < 10:
+        break
+    dh_turbo -= res
+
+
+# hp + dh_turbo = 0
+# pumped.h  = start.h - dh_turbo
+# 
+ 
+#pumped = H2(s=start.s, h=start.h+dh_pump)                # state 2
+#regen = H2(h=pumped.h+q_nozzle, P=pumped.P)             # state 3
+#turbo = H2(h=regen.h+dh_turbo, s=regen.s)               # state 4
+bypass = H2(h=regen.h+q_regen1, P=regen.P)              # state 6
+throt = H2(h=bypass.h, P=mix.P)                        # state 5
+
+
+
+
+
 
 
 states = [start, pumped, regen, turbo, bypass, throt, mix, hot, cfe, pm, core]
+print(len(states))
+flow1 = [start, pumped, regen, bypass, throt, mix, hot, cfe, pm, core]
+flow2 = [start, pumped, regen, turbo, mix, hot, cfe, pm, core]
+#plt.plot([point.s for point in flow1[:-1]], [point.T for point in flow1[:-1]])
+plt.plot([point.s for point in flow2[:-1]], [point.T for point in flow2[:-1]])
+i=11
+T=range(700, 1200)
+print("Max Pressure:", pumped.P)
+s = H2.s.table.func_t(P1=pumped.P)
+#plt.plot(s(T), T)
+for point in states[:-1]:
+    i -=1
+    plt.plot(point.s, point.T, '.', label=i)
 
 
-plt.plot([point.s for point in flow1], [point.T for point in flow1])
-plt.plot([point.s for point in flow2], [point.T for point in flow2])
-plt.plot([point.s for point in states], [point.T for point in states], '.')
+for point in states:
+    print(point.T, "|", point.s)
 plt.show()
 
 
--- fluids.py
+++ fluids.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os.path
 
 debug = False
+extra = False
 def dbp(*args):
     "Prints arguments if in debug mode"
     if debug:
@@ -15,57 +16,89 @@ class Table:
 
     def __init__(self, P: np.array, T: np.array, data: np.array):
         self.P_axis = P
-        self.T = T
+        self.T_axis = T
         self.csp_axis = []
         self.cst_axis = []
-        self.data = data
-        self.tol = 1e-5 # tolerance for removing duplicate values
-        
+        self.data = data            # fluid property values
+        self.tol = 1e-5             # tolerance for removing duplicate values
+        self.P_range = (min(self.P_axis), max(self.P_axis))
+        self.T_range = (min(self.T_axis), max(self.T_axis))
+        self.data_range = (np.min(self.data), np.max(self.data))
+
+
         # create spline fits as a function of pressure at specified temperatures
         self.cs_p = [] 
-        for i in range(len(self.T)):
+        for i in range(len(self.T_axis)):
             row = self.data[i,:]
             x = self.P_axis[~np.isnan(row)]
             y = row[~np.isnan(row)]
             if len(y) > 1:
-                self.cs_p.append(CubicSpline(x, y, extrapolate=False))
-                self.csp_axis.append(self.T[i])
+                self.cs_p.append(CubicSpline(x, y, extrapolate=extra))
+                self.csp_axis.append(self.T_axis[i])
             
         # create spline fits as a function of temperature at specified pressures    
         self.cs_t = []                   
         for j in range(len(self.P_axis)):
             column = self.data[:,j]
-            x = self.T[~np.isnan(column)]
+            x = self.T_axis[~np.isnan(column)]
             y = column[~np.isnan(column)]
             if len(y) > 1:
-                self.cs_t.append(CubicSpline(x, y, extrapolate=False))
+                self.cs_t.append(CubicSpline(x, y, extrapolate=extra))
                 self.cst_axis.append(self.P_axis[j])
-    
-    # get property value from pressure and temperature
+    def check_range(self, P1=None, T1=None, Y1=None):
+        "check if input property values are within range of table"
+        if P1 is not None:
+            if P1<self.P_range[0] or P1>self.P_range[1]:
+                raise ValueError(f"Pressure of {P1} is not within tabulated range of {self.P_range}")
+        if T1 is not None:
+            if T1<self.T_range[0] or T1>self.T_range[1]:
+                raise ValueError(f"Temperature of {T1} is not within tabulated range of {self.T_range}")
+        if Y1 is not None:
+            if Y1<self.data_range[0] or Y1>self.data_range[1]:
+                raise ValueError(f"Property value of {Y1} is not within tabulated range of {self.data_range}")
+
     def interp(self, P1, T1) -> float:
+        "Obtain property value from pressure and temperature"
+        self.check_range(P1=P1, T1=T1)
         FP = self.func_p(T1)
         FT = self.func_t(P1)
-        return (FP(P1) + FT(T1)) / 2
+        Ans = (FP(P1) + FT(T1)) / 2
+        if np.isnan(Ans):
+            raise ValueError("No solution found")
+        return Ans
     
-    # Property as a function of temperature at specified pressure P1
     def func_t(self, P1) -> PPoly:
+        "Property as a function of temperature at specified pressure P1"
+        self.check_range(P1=P1)
         at_p1 = []
+        tvals = []
         for i in range(len(self.csp_axis)):
-            at_p1.append(self.cs_p[i](P1))
+            yval = self.cs_p[i](P1)
+            if np.isfinite(yval):
+                at_p1.append(yval)
+                tvals.append(self.csp_axis[i])
 
-        return CubicSpline(self.csp_axis, at_p1, extrapolate=False)
+        return CubicSpline(tvals, at_p1, extrapolate=extra)
     
-    # Property as a function of pressure at specified temperature T1
+    
     def func_p(self, T1) -> PPoly:
+        "Property as a function of pressure at specified temperature T1"
+        self.check_range(T1=T1)
         at_t1 = []
+        pvals = []
         for j in range(len(self.cst_axis)):
-            at_t1.append(self.cs_t[j](T1))
+            yval = self.cs_t[j](T1)
+            if np.isfinite(yval):
+                at_t1.append(yval)
+                pvals.append(self.cst_axis[j])
 
-        return CubicSpline(self.cst_axis, at_t1, extrapolate=False)
+
+        return CubicSpline(pvals, at_t1, extrapolate=extra)
     
     
     def get_p(self, T1, Y1) -> float:
         '''get pressure value from given temperature and property value (not recommended for enthalpy or cp tables).'''
+        self.check_range(T1=T1, Y1=Y1)
         func = self.func_p(T1)
         solns = self.combine(func.solve(Y1))
         if len(solns)!=1:
@@ -75,11 +108,11 @@ class Table:
     
     def t_func_p(self,Y1) -> PPoly:
         '''Get temperature as a function of pressure for given property value'''
-
+        self.check_range(Y1=Y1)
         # find temperature at tabulated pressures and given property value Y1
         intercepts = dict()
         dbp(Y1)
-        dbp(self.T)
+        dbp(self.T_axis)
         for i in range(len(self.cs_t)):
             Ti = self.combine(self.cs_t[i].solve(Y1))
             dbp(Ti)
@@ -93,11 +126,12 @@ class Table:
         
         # return function T(P) created from interpolation of these points
         dbp(list(intercepts.keys()), list(intercepts.values()))
-        return CubicSpline(list(intercepts.keys()), list(intercepts.values()), extrapolate=False)
+        return CubicSpline(list(intercepts.keys()), list(intercepts.values()), extrapolate=extra)
     
     
     def get_t(self, P1, Y1) -> float:
         '''Get temperature from pressure and property value'''
+        self.check_range(P1=P1, Y1=Y1)
         func = self.func_t(P1)
         solns = self.combine(func.solve(Y1))
         if len(solns)!=1:
@@ -142,7 +176,7 @@ class FluidProperty:
             self.table = Table(P=data[0,1:].astype(float), T=data[1:,0].astype(float), data=data[1:,1:].astype(float))
 
     def knownas(self, string):
-        return (string.upper() in self.callnames)
+        return (string.upper().replace(" ", "_") in self.callnames)
 
     def __str__(self):
         return f"[{self.tag}] {self.name} ({self.units})"
@@ -187,27 +221,43 @@ class FluidState:
         if pt_count==0:
             self.double_solve(props[keys[0]], keys[0].table, props[keys[1]], keys[1].table)
         
-        
         # Solve for only one of either pressure or temperature if needed
         if pt_count == 1:
+            # Check if temperature was provided
             if self.fluid.T in keys:
-                #self.properties[self.fluid.T] = props[self.fluid.T]
+                # solve for pressure
+                self.properties[self.fluid.T] = props[self.fluid.T]
                 other_prop = keys[keys is not self.fluid.T]
                 self.properties[self.fluid.P] = other_prop.table.get_p( T1=props[self.fluid.T], Y1=props[other_prop])
+
+            # check if pressure was provided 
             elif self.fluid.P in keys:
+                # self for temperature
                 self.properties[self.fluid.P] = props[self.fluid.P]
                 other_prop = keys[keys is not self.fluid.P]
                 self.properties[self.fluid.T] = other_prop.table.get_t( P1=props[self.fluid.P], Y1=props[other_prop])
             else:
-                raise NameError("Uh Oh")
+                # neither property can be found but one was flagged as being provided in previous section
+                raise NameError("Solve Error: Could not locate provided pressure/temperature")
+        
+        # set value for each property
         for pr in self.fluid.properties:
             if pr in keys:
                 self.properties[pr] = props[pr]
             elif pr.known and (not pr.axis):
-                self.properties[pr] = pr.table.interp(P1=self.P, T1=self.T)
+                try:
+                    self.properties[pr] = pr.table.interp(P1=self.P, T1=self.T)
+                except KeyError as err:
+                    # TODO: FIXME
+                    
+                    print("FAILED:", err, pr.name, "P,T:", self.P, self.T)
+                    #print(pr.table.cs_t)
+                    self.properties[pr] = 0
+                    #self.properties[pr] = np.nan
     
     def double_solve(self, value1, table1, value2, table2) -> "float, float":
-        '''solve for both pressure and temperature as a function of other variables'''
+        '''solve for both pressure and temperature as a function of other variables
+        Should mostly only be called internally by other fluid state methods'''
 
         t1_p = table1.t_func_p(value1) # function T(P) given property 1
         t2_p = table2.t_func_p(value2) # function T(P) given property 2
@@ -259,15 +309,8 @@ class FluidState:
                 
     # control printout behavior of fluid state
     def __str__(self):
-        pstr = [f"Fluid state with properties:",
-                f"[P] Pressure: {self.P}",
-                f"[T] Temperature: {self.T}",
-                f"[h] Enthalpy: {self.h}",
-                f"[r] Density: {self.r}",
-                f"[v] Specific Volume: {self.v}",
-                f"[s] Entropy: {self.s}",
-                f"[cp] Specific Heat: {self.cp}"]
-        pstr = ["Fluid state with properties:"] + [f"{p}: {self.properties[p]}" for p in self.fluid.properties]
+
+        pstr = ["Fluid state with properties:"] + [f"{p}: {self.properties[p]}" for p in self.properties.keys()]
         return "\n\t".join(pstr)
 
 class Fluid:
@@ -278,16 +321,21 @@ class Fluid:
         self.properties = [FluidProperty(tag='p', name='Pressure', alt=('PRESS'), rev=False, axis=True),    # Pressure
             FluidProperty(tag='t', name='Temperature', alt=('TEMP'), rev=False, axis=True),                 # Temperature
             FluidProperty(tag='h', name='Enthalpy', alt=('ENTH'), rev=True),                                # Enthalpy
+            FluidProperty(tag='u', name='Internal Energy', alt=(), rev=True),                               # Internal Energy
             FluidProperty(tag='v', name='Specific Volume', alt=('VOLUME', 'VOL'), rev=True),                # Specific volume
             FluidProperty(tag='rho', name='Density', alt=('R'), rev=True),                                  # Density
             FluidProperty(tag='s', name='Entropy', alt=('S', 'ENTROPY'), rev=True),                         # Entropy
             FluidProperty(tag='cp', name='Specific Heat', alt=('C'), rev=False),                            # Specific heat
             FluidProperty(tag='gamma', name='Specific Heat Ratio', alt=('Y'), rev=False),                   # gamma, sp. heat ratio
             FluidProperty(tag='nu', name='Kinematic Viscosity', alt=('VISC', 'VISCOSITY'), rev=False),      # viscosity (kinematic)
-            FluidProperty(tag='a', name='Speed of Sound', alt=(), rev=False)]                               # speed of sound
+            FluidProperty(tag='a', name='Speed of Sound', alt=(), rev=False),                               # speed of sound
+            FluidProperty(tag='gibbs', name='Gibbs Free Energy', alt=('GFE'), rev=False),                   # gibbs free energy
+            FluidProperty(tag='k', name='Thermal Conductivity', alt=("CONDUCTIVITY"), rev=False),                         # thermal conductivity
+            FluidProperty(tag='m', name='Molecular Mass', alt=("MOLAR_MASS"), rev=False)]                   # molecular mass
         for prop, fname in prop_tables.items():
             for p in self.properties:
                 if p.knownas(prop):
+                    #print(p)
                     p.read_table(fname)
         
     
@@ -299,7 +347,7 @@ class Fluid:
         return self.state(**kwargs)
 
 
-    def __getattr__(self, __name: str): 
+    def __getattr__(self, __name: str):
         for p in self.properties:
             if p.knownas(__name):
                 return p
@@ -330,7 +378,6 @@ class Fluid:
             raise ValueError("Input properties not recognized")
         return FluidState(self, input_props)
 
-
 class Process:
     def __init__(self, state1, **kwargs):
         self.isentropic = None
@@ -343,8 +390,8 @@ class Process:
         pass
 
 
-dir = "CEA Property Tables"
-
+#dir = "CEA Property Tables"
+dir = "Updated"
 prop_files = dict()
 for file in os.listdir(dir):
     tag = file.split(".")[0]
