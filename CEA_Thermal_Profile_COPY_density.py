75a7b64622a919261f1f06d0047f8a6c8f93127f
dhruv1017 <32229931+dhruv1017@users.noreply.github.com>
2023-01-26 11:35:08 -0500

    PM npz files

--- CEA_Thermal_Profile_COPY_density.py
+++ CEA_Thermal_Profile_COPY_density.py
@@ -371,33 +371,132 @@ if __name__ =="__main__":
     q_tp = "10000000 + 150**(100*r)"
     BCs = ([1494,"dirichlet"],[0,"neumann"])
 
-    r1 = .03
-    r2 = .045
-    P_core = 10
-    N = 7000
-    mdot = .108
-    L = .84
-    num_cells = 500
-    D_1 = CFE(r2,r2-r1,L,7, num_cells, T_P, mdot, N, P_core,BCs,PS_data_1,OG_power=7)
+    from fluids import H2
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    mpl.rc('font', family='Times New Roman',size="10")
+    mpl.rc('figure', figsize=(4.8,3.6))
+    mpl.rc('savefig', dpi=800)
+    mpl.rc('lines', linewidth=1.2)
+    mpl.rc('axes', grid=True)
+    mpl.rc('grid', linewidth=0.25)
+    mpl.rc('mathtext', fontset="dejavuserif")
+    mpl.rc('xtick.minor', visible=True, size=1.5, width=0.5)
+    mpl.rc('ytick.minor', visible=True, size=1.5, width=0.5)
+    plt.rcParams['figure.constrained_layout.use'] =  True
 
-    R_1 = solve(D_1)
-    T_1 = R_1[0]
-    M_1 = R_1[1]
-    X_1 = R_1[2]
-    QR_1 = R_1[3]
-    Q_1 = R_1[4]
+    base = {"P_core":10e6,
+            "T_channel":450,
+            "r5":56e-3,
+            "d56":8e-3,
+            "N":7000,
+            "nu_s":0.691,
+            "L_CFE":.84,
+            "T_core":3700}
 
-    x_1 = np.array(D_1.cell_center_radii)
+    stdlim = [0.5, 2]
 
-    temp = T_1
-    radius = np.array(x_1)
-    void = np.array(X_1)
-    fuel_density = np.array(M_1[5])
-    prop_density = np.array(R_1[5])
+    # ******************************************
+    # TODO: if you don't want to plot vs a particular parameter, remove it from `vary`
+    # TODO: if you want to plot a particular parameter over a range different from others, replace its limits in `vary`
+    # ******************************************
 
-    mix_density = fuel_density * (1 - void) + prop_density * void
-    np.save(f"density_{num_cells}CEA.npy", mix_density)
-    np.save(f"radius_{num_cells}CEA.npy", radius)
+    vary = {"P_core":stdlim,
+            # "T_channel":stdlim,
+            # "r5":stdlim,
+            # "d56":[0.125, 2],
+            "N":stdlim,
+            # "nu_s":stdlim,
+            "L_CFE":stdlim}
+
+    labels = {"P_core":"core pressure $P_3$", 
+            "T_channel":"channel temperature $T_1$",
+            "r5":"case radius $r_5$",
+            "d56":"outer channel width $(r_6 - r_5)$",
+            "N":"CFE rotation rate $\omega$",
+            "nu_s":"turbine sp. speed $\\nu_s$",
+            "L_CFE":"CFE length $l$"}
+
+    yvals = dict()
+    xvals = dict()
+
+    base_core = H2(P=base["P_core"], T=base["T_core"])      # speed code up by not calculating on every single loop
+
+    for key in vary:                    # iterate through properties we want to vary
+        props = base.copy()             # reset all properties to base values
+        lim = vary[key]                 # relative property value limits
+        n_pts = 50                      # number of x-value points to plot to form a smooth curve
+        xvals[key] = np.arange(lim[0], lim[1]+1e-5, np.diff(lim)/n_pts)
+        yvals[key] = []
+        i = 0                           # counter
+        for x in xvals[key]:
+            props[key] = x * base[key]              # adjust single parameter
+            L_total = props["L_CFE"] + 0.1          # compute total length
+            if key=="P_core" or key=="T_core":      # check if core state needs adjustment
+                core = H2(P=props["P_core"], T=props["T_core"])
+            else:
+                core = base_core
+            omega = props["N"] * np.pi/30           # compute omega
+            r6 = props["r5"] + props["d56"]         # compute r6
+            i +=1
+
+            # ******************************************
+            y=1; print("CHANGE THIS LINE!")
+            # TODO: ADD YOUR CALCULATIONS FOR THE OUTPUT Y-AXIS VALUE BASED ON INPUTS
+            # TODO: SET y=<YOUR Y-VALUE PROPERTY>
+            # ******************************************
+            
+            r1 = .03
+            r2 = .045
+            mdot = .108
+            num_cells = 4
+            P_core = props["P_core"]
+            
+            N = props["N"]
+            L = props["L_CFE"]
+            D_1 = CFE(r2,r2-r1,L,7, num_cells, T_P, mdot, N, P_core,BCs,PS_data_1,OG_power=7)
+
+            R_1 = solve(D_1)
+            T_1 = R_1[0]
+            M_1 = R_1[1]
+            X_1 = R_1[2]
+            QR_1 = R_1[3]
+            Q_1 = R_1[4]
+
+            x_1 = np.array(D_1.cell_center_radii)
+
+            temp = T_1
+            radius = np.array(x_1)
+            void = np.array(X_1)
+            fuel_density = np.array(M_1[5])
+            prop_density = np.array(R_1[5])
+
+            mix_density = fuel_density * (1 - void) + prop_density * void
+            np.save(f"PM_PressureDrop/density_CEA{key}_{i}.npy", mix_density)
+            np.save(f"PM_PressureDrop/radius_CEA{key}_{i}.npy", radius)
+
+            yvals[key].append(y)
+
+
+    for key in yvals:       # plot each line
+        plt.plot(yvals[key], label=labels[key])
+
+    plt.legend()
+    plt.xlim(0,2.2)
+
+    # ******************************************
+    # TODO: update figure name
+    # ******************************************
+
+    plt.savefig("FIGURENAME.svg")
+    plt.show()
+
+
+
+        
+
+    
 
     print("Design 1 Max Temp:",np.amax(T_1))
     print("Design 1 Max Void:",np.amax(X_1))
