4f062d63bd63b7b3439f483b4d78828b3c997336
David Austin <114694180+daa97@users.noreply.github.com>
2023-02-01 13:54:19 -0500

    finalized figures with updated radius limits

--- testforhs.py
+++ testforhs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import turbine_design_baines as tdb
+import prop_vary as pv
 # etas = [0.78, 0.80, 0.82, 0.84, 0.85 0.86]
 # nus = range(0.15,1,0.01)
 # for eta in etas:
@@ -38,7 +39,7 @@ static_cfe_inputs = {
     "mass_flow" : 0.108, #CFE channel mass flow rate [kg s^-1]
     "uranium_mass":m_U["baseline"],
     "temp" : 450, #[K]
-    "press" : 13.754 #MPa - Turbine Inlet Pressure
+    "press" : P1["baseline"]/1e6 #MPa - Turbine Inlet Pressure
 } 
 
 dynamic_turb_inputs = {
@@ -49,12 +50,12 @@ dynamic_turb_inputs = {
     "v_s" : 0.695
 }
 
-# def get_props(turb):
-#     copy = dict()
-#     for k,v in turb.__dict__.items():
-#         if isinstance(v, (str, int, float, np.ndarray)):
-#             copy[k] = v
-#     return copy
+def get_props(turb):
+    copy = dict()
+    for k,v in turb.__dict__.items():
+        if isinstance(v, (str, int, float, np.ndarray)):
+            copy[k] = v
+    return copy
 
 def find_turbine(static_inputs, dynamic_inputs, dict_only=False):
     test_cfe = tdb.CFE(static_inputs,dynamic_inputs,1)
@@ -71,36 +72,29 @@ if __name__=="__main__":
     print(test_turb)
     # test_turb.make_hub_and_shroud()
 
-# noz.create_cascade()
-# noz.find_setting_angle()
-# noz_out = noz.calc_naca_profile()
-# nozx = noz_out[0]
-# nozy = noz_out[1]
-# nozchi = noz_out[2]
-# nozsuc = noz_out[3]
-# nozpres = noz_out[4]
-# r_2 = noz.t_2c/2 * noz.c
-# r_3 = noz.t_3c/2 * noz.c
-# thetas = np.linspace(0, 2 * np.pi, num = 100)
-# le = np.zeros((100,2))
-# te = np.zeros((100,2))
-# for i,theta in enumerate(thetas):
-#     le[i,0] = r_2 * (np.cos(theta))
-#     le[i,1] = r_2 * (np.sin(theta))
-#     te[i,0] = r_3 * (np.cos(theta)) + nozx[-1]
-#     te[i,1] = r_3 * (np.sin(theta)) + nozy[-1]
-# plt.plot(le[:,0],le[:,1])
-# plt.plot(te[:,0],te[:,1])
-# plt.plot(nozx,nozy,marker = ".")
-# plt.plot(nozpres[:,0],nozpres[:,1],marker = ".")
-# plt.plot(nozsuc[:,0],nozsuc[:,1],marker = ".")
-# # plt.xlim([-0.01,1.01])
-# # plt.ylim([-0.1,0.1])
-# plt.show()
-# test_cfe = tdb.CFE(static_cfe_inputs,dynamic_turb_inputs,1)
-
-# init_turb = tdb.turbine(test_cfe,test_cfe.static_turb_inputs,dynamic_turb_inputs,1)
-
-# # test_turb = tdb.find_turb(test_cfe,init_turb)
-# # test_turb.print_turbine(opts)
-# init_turb.make_hub_and_shroud()
+    # noz.create_cascade()
+    # noz.find_setting_angle()
+    # noz_out = noz.calc_naca_profile()
+    # nozx = noz_out[0]
+    # nozy = noz_out[1]
+    # nozchi = noz_out[2]
+    # nozsuc = noz_out[3]
+    # nozpres = noz_out[4]
+    # r_2 = noz.t_2c/2 * noz.c
+    # r_3 = noz.t_3c/2 * noz.c
+    # thetas = np.linspace(0, 2 * np.pi, num = 100)
+    # le = np.zeros((100,2))
+    # te = np.zeros((100,2))
+    # for i,theta in enumerate(thetas):
+    #     le[i,0] = r_2 * (np.cos(theta))
+    #     le[i,1] = r_2 * (np.sin(theta))
+    #     te[i,0] = r_3 * (np.cos(theta)) + nozx[-1]
+    #     te[i,1] = r_3 * (np.sin(theta)) + nozy[-1]
+    # plt.plot(le[:,0],le[:,1])
+    # plt.plot(te[:,0],te[:,1])
+    # plt.plot(nozx,nozy,marker = ".")
+    # plt.plot(nozpres[:,0],nozpres[:,1],marker = ".")
+    # plt.plot(nozsuc[:,0],nozsuc[:,1],marker = ".")
+    # # plt.xlim([-0.01,1.01])
+    # # plt.ylim([-0.1,0.1])
+    # plt.show()
